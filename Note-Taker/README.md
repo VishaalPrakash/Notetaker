89960bf5b8750da30bd79931a7d0b0cf7486855a
Dark Tower <prakashvishaal@gmail.com>
2022-06-05 00:09:26 -0600

    completed

---
+++ Note-Taker/README.md
@@ -0,0 +1,29 @@
+# Challenge 11 - Express Note Taker
+
+This application is a note taker that is created by using express.js.
+
+
+## Installation
+
+User must install node_modules.
+
+## Usage
+
+User can open localserver on PORT:3001 by inputting <node server.js>.
+
+## Built with
+* HTML
+* CSS
+* Node.js
+* Express.js
+
+## Website
+GitHub: https://github.com/jimbn/Challenge-11-Express-Note-Taker
+
+Heroku: https://cryptic-sea-88844.herokuapp.com/
+
+## Screenshot
+![Screenshot of Application](assets/screenshots/screenshot-01.png)
+
+## Contribution
+Jim Nguyen
---
+++ Note-Taker/routes/apiRoutes/dbRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const db = require('../../db/db.json');
+const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+
+router.get('/notes', (req,res) => res.json(db));
+
+router.post('/notes', (req,res) => {
+    let newId = uuid.v4();
+    let toSave = {
+        id: newId,
+        title: req.body.title,
+        text: req.body.text,
+    }
+    db.push(toSave);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(db, null, 2)
+    );
+    res.send(db);
+});
+
+router.delete('/notes/:id', (req,res) => {
+    let noteId = req.params.id;
+    let filteredNotes = db.filter((db) => db.id != noteId);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(filteredNotes, null, 2)
+    );
+    res.send(filteredNotes);
+});
+
+module.exports = router;
